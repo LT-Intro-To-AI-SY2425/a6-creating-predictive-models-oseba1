ac6f54389a9eace36fa7a65dbf11b16b65160623
Max Maiers <maxmaiers@gmail.com>
2024-12-03 11:34:19 -0600

    almost done

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -8,17 +8,47 @@ x = data["Age"].values
 y = data["Blood Pressure"].values
 
 # Use reshape to turn the x values into 2D arrays:
-x = x.reshape(-1,1)
+
+x = x.reshape(-1, 1)
 
 # Create the model
 
-# Find the coefficient, bias, and r squared values. 
-# Each should be a float and rounded to two decimal places. 
+model = LinearRegression().fit(x, y)
 
+# labels axes and creates scatterplot
+
+plt.xlabel("Age")
+plt.ylabel("Blood Pressure")
+plt.title("Blood Pressure vs. Age")
+
+# Find the coefficient, bias, and r squared values.
+
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
+
+
+# Each should be a float and rounded to two decimal places. 
 
 # Print out the linear equation and r squared value
 
-# Predict the the blood pressure of someone who is 43 years old.
-# Print out the prediction
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
 
-# Create the model in matplotlib and include the line of best fit
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+### Predict the the blood pressure of someone who is 43 years old.
+
+prediction = model.predict([[43]])
+
+print(f"Prediction when x is {43}: {prediction}")
+
+### Print out the prediction
+
+### Create the model in matplotlib and include the line of best fit
+
+plt.scatter(x,y, c="blue")
+plt.scatter(43, prediction, c="purple")
+
+plt.legend()
+plt.show()
--- part1-linear-regression/part1_writeup.md
+++ part1-linear-regression/part1_writeup.md
@@ -6,6 +6,12 @@ After completing `a6_part1.py` answer the following questions
 
 1. What is the r squared value?  What does this say about this linear regression model?
 
+The r squared value is a value that represents how well the line of best fit represents the data. The closer to one the value is the better it represents the data.
+
 2. According to your model, what is the predicted systolic blood pressure for a person who is 42 years old?
 
+The predicted value according to the model is 137.46.
+
 3. How accurate do you think this model's predictions are?  Do you think this model is accurate enough to reliably predict someone's blood pressure based on their age?  Why or why not?  And if not, what could improve the model?
+
+It is a fairly decent prediction 
