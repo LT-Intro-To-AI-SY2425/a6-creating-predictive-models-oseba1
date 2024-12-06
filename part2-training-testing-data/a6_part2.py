df49d495430de27d44d5dba235749c006993400d
Max Maiers <maxmaiers@gmail.com>
2024-12-06 11:34:42 -0600

    changes

--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -12,6 +12,43 @@ data = pd.read_csv("part2-training-testing-data/blood_pressure_data.csv")
 x = data["Age"].values
 y = data["Blood Pressure"].values
 
+x = x.reshape(-1, 1)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+model = LinearRegression().fit(x_train, y_train)
+
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+rSquared = model.score(x_train, x_train)
+
+print("Model's Linear Equation: y=",coef, "x+", intercept)
+print("R Squared value:", rSquared)
+
+predict = model.predict(x_test)
+
+predict = np.around(predict, 2)
+
+for index in range(len(x_test)):
+    actual = y_test[index]
+    predicted_y = predict[index]
+    x_coord = x_test[index]
+
+plt.figure(figsize=(5,4))
+
+plt.scatter(x_train,y_train, c="blue", label="Training Data")
+plt.scatter(x_test, y_test, c="purple", label="Testing Data")
+
+plt.scatter(x_test, predict, c="red", label="Predictions")
+
+plt.xlabel("Age")
+plt.ylabel("Blood Pressure")
+plt.title("Blood Pressure by Age")
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show()
+
 # Create your training and testing datasets:
 
 # Use reshape to turn the x values into 2D arrays:
--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
@@ -9,14 +10,45 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
+x = x.reshape(-1, 1)
+
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, 0.2)
 
 #create linear regression model
+model = LinearRegression().fit(xtrain, ytrain)
 
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
 
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+rSquared = model.score(xtrain, ytrain)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
+
+predict = model.predict(xtest)
+predit = np.around(predict, 2)
+
+for index in range(len(xtest)):
+    actual = ytest[index]
+    predicted_y = predict[index]
+    x_coord = xtest[index]
+
+plt.figure(figsize=(5,4))
+
+plt.scatter(xtrain, ytrain, c="blue", label="Training Data")
+plt.scatter(xtest, ytest, c="purple", label="Testing Data")
+
+plt.scatter(xtest, predict, c="red", label="Predictions")
+
+plt.xlabel("Car")
+plt.ylabel("Price")
+plt.title("Price by Car")
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show()
+
 print("***************")
 print("Testing Results")
