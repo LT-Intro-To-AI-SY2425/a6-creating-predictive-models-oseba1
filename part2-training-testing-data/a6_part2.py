4ee81d6e8f489f4505da9eae37ca3e52ce15729f
Max Maiers <maxmaiers@gmail.com>
2024-12-10 11:31:53 -0600

    changes

--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -20,7 +20,7 @@ model = LinearRegression().fit(x_train, y_train)
 
 coef = round(float(model.coef_[0]), 2)
 intercept = round(float(model.intercept_), 2)
-rSquared = model.score(x_train, x_train)
+rSquared = model.score(x_train, y_train)
 
 print("Model's Linear Equation: y=",coef, "x+", intercept)
 print("R Squared value:", rSquared)
